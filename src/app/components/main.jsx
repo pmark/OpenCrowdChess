d8846955a5c7659d5dca483b370734d64433126b
P. Mark Anderson <manderson@flightstats.com>
2015-11-06 13:40:55 -0800

    import

---
+++ src/app/components/main.jsx
@@ -0,0 +1,68 @@
+/** In this file, we create a React component which incorporates components provided by material-ui */
+
+const React = require('react');
+const RaisedButton = require('material-ui/lib/raised-button');
+const AppBar = require('material-ui/lib/app-bar');
+const Dialog = require('material-ui/lib/dialog');
+const ThemeManager = require('material-ui/lib/styles/theme-manager');
+const LightRawTheme = require('material-ui/lib/styles/raw-themes/light-raw-theme');
+const Colors = require('material-ui/lib/styles/colors');
+
+const Main = React.createClass({
+
+  childContextTypes: {
+    muiTheme: React.PropTypes.object,
+  },
+
+  getInitialState () {
+    return {
+      muiTheme: ThemeManager.getMuiTheme(LightRawTheme),
+    };
+  },
+
+  getChildContext() {
+    return {
+      muiTheme: this.state.muiTheme,
+    };
+  },
+
+  componentWillMount() {
+    let newMuiTheme = ThemeManager.modifyRawThemePalette(this.state.muiTheme, {
+      accent1Color: Colors.deepOrange500,
+    });
+    
+    this.setState({muiTheme: newMuiTheme});
+  },
+
+  render() {
+
+    let containerStyle = {
+      textAlign: 'center',
+      paddingTop: '10px',
+    };
+
+    return (
+      <div style={containerStyle}>
+        <AppBar
+          title="The chess chief is: Johnny Knight"
+          showMenuIconButton={false}
+          iconClassNameRight="muidocs-icon-navigation-expand-more" />
+
+        <div id={"board"} style={{width: '35%', marginTop:'10px'}}></div>
+
+        <h1>25 watchers, 12 players</h1>
+        <h2>Move a piece to start playing.</h2>
+
+        <RaisedButton label="Button" primary={true} onTouchTap={this._handleTouchTap} />
+
+      </div>
+    );
+  },
+
+  _handleTouchTap() {
+    this.refs.superSecretPasswordDialog.show();
+  },
+
+});
+
+module.exports = Main;
